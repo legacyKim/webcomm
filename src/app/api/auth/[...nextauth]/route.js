5823bebbed9197b326e69db1cf4c79d64a43a760
unknown <arbam486@gmail.com>
2025-06-30 08:25:29 +0900

    dep err

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,40 +1,40 @@
-import NextAuth from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import prisma from '@/app/lib/prisma'
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "../../../../app/lib/prisma";
 
 export const authOptions = {
   providers: [
     CredentialsProvider({
-      name: 'Credentials',
+      name: "Credentials",
       credentials: {
         username: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" }
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
         const user = await prisma.member.findUnique({
           where: {
-            user_id: credentials.username
-          }
-        })
+            user_id: credentials.username,
+          },
+        });
 
         if (user && user.password === credentials.password) {
           return {
             id: user.user_id,
             name: user.user_nickname,
-            email: user.email
-          }
+            email: user.email,
+          };
         }
-        return null
-      }
-    })
+        return null;
+      },
+    }),
   ],
   session: {
-    strategy: "jwt"
+    strategy: "jwt",
   },
   pages: {
-    signIn: '/login'
-  }
-}
+    signIn: "/login",
+  },
+};
 
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST } 
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
