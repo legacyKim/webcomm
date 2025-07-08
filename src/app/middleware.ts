ade1ae34b746d35c6a286fbb488484f5eff01b88
unknown <arbam486@gmail.com>
2025-07-09 08:20:32 +0900

    for employ

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -21,7 +21,7 @@ export function middleware(req: NextRequest) {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET!);
     let authority: number | undefined = undefined;
-    let authorityRaw: number | undefined = undefined;
+    const authorityRaw: number | undefined = undefined;
 
     console.log("decoded:", decoded);
     console.log("authority:", authority, typeof authority);
