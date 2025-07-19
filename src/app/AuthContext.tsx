d9694b9dc8a7493dfefc632be1a914375b45a9d3
unknown <arbam486@gmail.com>
2025-07-19 19:14:54 +0900

    dep

--- src/app/AuthContext.tsx
+++ src/app/AuthContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { AuthContextType } from "@/type/type";
 import { Posts } from "@/type/type";
 
