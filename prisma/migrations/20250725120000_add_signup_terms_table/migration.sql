48c17f16b9237105bdfd7d73c4580bec0b36d5ed
unknown <arbam486@gmail.com>
2025-07-25 19:26:43 +0900

    dep

---
+++ prisma/migrations/20250725120000_add_signup_terms_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "signup_terms" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "signup_terms_pkey" PRIMARY KEY ("id")
+);
