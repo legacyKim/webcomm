0e698782c3dd7b9bd6f15a2562aaa6e91836babb
unknown <arbam486@gmail.com>
2025-07-27 16:58:56 +0900

    migration

--- prisma/migrations/20250727075449_re/migration.sql
+++ prisma/migrations/20250727075449_re/migration.sql
@@ -7,6 +7,3 @@
 -- AlterTable
 ALTER TABLE "members" ADD COLUMN     "marketing_enabled" BOOLEAN NOT NULL DEFAULT false,
 ADD COLUMN     "notification_enabled" BOOLEAN NOT NULL DEFAULT false;
-
--- CreateIndex
-CREATE UNIQUE INDEX "push_subscriptions_user_id_endpoint_key" ON "push_subscriptions"("user_id", "endpoint");
