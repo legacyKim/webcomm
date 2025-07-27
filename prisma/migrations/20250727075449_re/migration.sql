d5fb3c04c2869519f04ce518c6d974c4f25a98d0
unknown <arbam486@gmail.com>
2025-07-27 16:57:13 +0900

    db migration

--- prisma/migrations/20250727075449_re/migration.sql
+++ prisma/migrations/20250727075449_re/migration.sql
@@ -8,15 +8,5 @@
 ALTER TABLE "members" ADD COLUMN     "marketing_enabled" BOOLEAN NOT NULL DEFAULT false,
 ADD COLUMN     "notification_enabled" BOOLEAN NOT NULL DEFAULT false;
 
--- CreateTable
-CREATE TABLE "signup_terms" (
-    "id" SERIAL NOT NULL,
-    "content" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "signup_terms_pkey" PRIMARY KEY ("id")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "push_subscriptions_user_id_endpoint_key" ON "push_subscriptions"("user_id", "endpoint");
